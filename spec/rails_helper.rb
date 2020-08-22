8bca89fd5f1cbd1038566a93eccef071bf6318e9
taha <mohamedtaha.boutayeb@gmail.com>
2020-08-22 18:43:11 +0100

    chore: setup authentication & authorization

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,8 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
-
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
 
   config.infer_spec_type_from_file_location!
