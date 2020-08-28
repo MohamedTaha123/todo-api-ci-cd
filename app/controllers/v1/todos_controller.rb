ed5a0d2bf628e9ad06e906b3a4329266d607a869
taha <mohamedtaha.boutayeb@gmail.com>
2020-08-28 17:40:26 +0100

    chore: fix n+1 performance issue

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -4,7 +4,7 @@ module V1
 
   # GET /todos
   def index
-    @todos = current_user.todos.paginate(page: params[:page], per_page: 20)
+    @todos = current_user.todos.includes([:user]).paginate(page: params[:page], per_page: 20)
     json_response(@todos)
   end
 
